469338ada245e1ea0a4719dbe05b608b5338463b
Kaushalya M.K.B <86149391+IT20118372@users.noreply.github.com>
2021-09-07 15:30:17 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = {
+    products: [
+        {id: '1',
+         pname: 'item 1',
+         price: 100  ,
+         image: 'https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.iimagez.com%2Fgreetings%2Fgood-night%2Fby-good-night-image&psig=AOvVaw00Kr2K7WhVIXUOWy4zDJF4&ust=1631068607421000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNCXl5rq6_ICFQAAAAAdAAAAABAD'
+   
+    },
+
+    {id: '2',
+         pname: 'item 2',
+         price: 100  ,
+         image: 'https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.iimagez.com%2Fgreetings%2Fgood-night%2Fby-good-night-image&psig=AOvVaw00Kr2K7WhVIXUOWy4zDJF4&ust=1631068607421000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNCXl5rq6_ICFQAAAAAdAAAAABAD'
+   
+    }, 
+    {id: '3',
+    pname: 'item 3',
+    price: 100  ,
+    image: 'https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.iimagez.com%2Fgreetings%2Fgood-night%2Fby-good-night-image&psig=AOvVaw00Kr2K7WhVIXUOWy4zDJF4&ust=1631068607421000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNCXl5rq6_ICFQAAAAAdAAAAABAD'
+
+}
+    ]
+}
+export default data;
