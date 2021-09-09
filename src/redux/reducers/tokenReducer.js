c056e1dd0f007ec6a71aca30173917723f0b026b
Kaushalya M.K.B <86149391+IT20118372@users.noreply.github.com>
2021-09-09 07:37:47 +0530

    Add files via upload

---
+++ src/redux/reducers/tokenReducer.js
@@ -0,0 +1,16 @@
+import { action } from 'commander'
+import ACTIONS from  '../actions'
+
+
+const token = ''
+
+const tokenReducer = (state = token, action) => {
+    switch(action.type){
+        case ACTIONS.GET_TOKEN:
+            return action.payload
+        default:
+            return state
+    }
+}
+
+export default tokenReducer
